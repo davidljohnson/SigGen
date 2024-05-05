b5357220a7be197c3aba566cabe3d44a2fd65209
Dave Johnson <21146456+davidljohnson@users.noreply.github.com>
2024-05-05 08:13:02 -0700

    Modify to only clone the SigmaHQ repo instead of update

--- update_sigma_rules.sh
+++ update_sigma_rules.sh
@@ -4,18 +4,8 @@
 SIGMA_REPO_DIR="./sigma"
 YAML_OUTPUT_DIR="./all_sigma_rules"
 
-# Ensure directories are created at the right place
-mkdir -p "$SIGMA_REPO_DIR"
-mkdir -p "$YAML_OUTPUT_DIR"
-
-# Clone or update the Sigma repository
-if [ ! -d "$SIGMA_REPO_DIR" ]; then
-    git clone https://github.com/SigmaHQ/sigma.git "$SIGMA_REPO_DIR"
-else
-    cd "$SIGMA_REPO_DIR"
-    git pull origin master
-    cd ..
-fi
+# Clone the Sigma repository (TODO: add update option)
+git clone https://github.com/SigmaHQ/sigma.git "$SIGMA_REPO_DIR"
 
 # Copy yml files to the output directory
 declare -a directories=("rules" "rules-compliance" "rules-dfir" "rules-emerging-threats" "rules-placeholder" "rules-threat-hunting")
